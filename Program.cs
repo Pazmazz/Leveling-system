97d4de3caa89eae436de012a6fd10f5d2286d8f0
Pazmaz <162527327+Pazmazz@users.noreply.github.com>
2024-12-05 18:31:23 -0500

    Added basic level system

--- Program.cs
+++ Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Unit
+{
+    Random rnd = new Random();
+    
+    public int hp = 20;
+    public int atk = 5;
+    public int def = 3;
+
+    public int exp = 0;
+    public int lvl = 1;
+
+    public void LevelUp()
+    {
+        int randomHp = rnd.Next(4, 7);
+        int randomAtk = rnd.Next(1, 4);
+        int randomDef = rnd.Next(1, 4);
+
+        hp += randomHp;
+        atk += randomAtk;
+        def += randomDef;
+        lvl += 1;
+
+        Console.WriteLine("You levled up!\nYou are now level " + lvl);
+        Console.WriteLine("Hp increased by " + randomHp + "!\nHP: " + hp);
+        Console.WriteLine("Attack increased by " + randomAtk + "!\nAttack: " + atk);
+        Console.WriteLine("Defense increased by " + randomDef + "!\nDefense: " + def);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Unit unit = new Unit();
+        
+        while (Console.ReadKey().Key == ConsoleKey.Enter)
+        {
+            Console.WriteLine("Level " + unit.lvl);
+            unit.exp += 1;
+            Console.WriteLine("Experience Points: " + unit.exp);
+            Console.WriteLine("");
+            
+            if (unit.exp % 5 == 0)
+            {
+                unit.LevelUp();
+            }
+        }
+
+    }
+}
