3ce6ad98317b4f9e218dedf61ec611e79b934d8b
Pazmazz <jaylendsmall@outlook.com>
2024-12-07 16:16:49 -0500

    Added exp scaling formula

--- Program.cs
+++ Program.cs
@@ -1,33 +1,41 @@
 ﻿class Unit
 {
     Random rnd = new Random();
-    
+
     public int hp = 20;
     public int atk = 5;
     public int def = 3;
 
     public int exp = 0;
+    public int totalExp = 0;
     int requiredExp = 0;
-    readonly int maxLvl = 20;
+    public readonly int maxLvl = 20;
     public int lvl = 1;
 
     public int GetRequiredExp(int lvl)
     {
-        requiredExp = Lerp(0, maxLvl, lvl);
+        requiredExp = (int)Math.Pow(lvl, 4) + 14;
         return requiredExp;
     }
 
-    public int GainExp(int num)
+    public void GainExp(int num)
     {
+        totalExp += num;
         exp += num;
 
         if (exp >= requiredExp)
         {
             while (exp >= requiredExp)
             {
-
+                exp -= requiredExp;
+                LevelUp();
             }
         }
+    }
+
+    public void CalculateRequiredExp()
+    {
+        requiredExp = GetRequiredExp(lvl);
     }
 
     public void LevelUp()
@@ -41,31 +49,38 @@
         def += randomDef;
         lvl++;
 
-        Console.WriteLine("You levled up!\nYou are now level " + lvl);
-        Console.WriteLine("Hp increased by " + randomHp + "!\nHP: " + hp);
-        Console.WriteLine("Attack increased by " + randomAtk + "!\nAttack: " + atk);
-        Console.WriteLine("Defense increased by " + randomDef + "!\nDefense: " + def);
+        Console.WriteLine("You leveled up!\nYou are now level " + lvl);
+        //Console.WriteLine("Hp increased by " + randomHp + "!\nHP: " + hp);
+        //Console.WriteLine("Attack increased by " + randomAtk + "!\nAttack: " + atk);
+        //Console.WriteLine("Defense increased by " + randomDef + "!\nDefense: " + def);
     }
 }
 
 class Program
 {
+    static void Yeild()
+    {
+        Console.ReadLine();
+    }
+    
     static void Main(string[] args)
     {
         Unit unit = new Unit();
         
         while (Console.ReadKey().Key == ConsoleKey.Enter)
         {
-            Console.WriteLine("Level " + unit.lvl);
-            unit.exp += 1;
-            // unit.exp = 552.4198 * Math.pow(1.22878, unit.lvl);
-            Console.WriteLine("Experience Points: " + unit.exp);
-            Console.WriteLine("");
-            
-            if (unit.exp % 5 == 0)
+            unit.CalculateRequiredExp();
+
+            if (unit.lvl == unit.maxLvl)
             {
-                unit.LevelUp();
+                Console.WriteLine("You maxed out!");
+                break;
             }
+            
+            unit.GainExp(15);
+            Console.WriteLine("Level " + unit.lvl);
+            Console.WriteLine("Experience Points: " + unit.totalExp);
+            Console.WriteLine("");
         }
 
     }
