5c3e3f119e7ff20a6a5ba03a7afde9c2add27484
Pazmazz <jaylendsmall@outlook.com>
2024-12-08 15:01:53 -0500

    Added more comments

--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
-﻿class Unit
+﻿/* 
+     All the number here are ajustable for testing purposes 
+*/
+
+class Unit
 {
     Random rnd = new Random();
 
@@ -12,12 +16,22 @@
     public readonly int maxLvl = 20;
     public int lvl = 1;
 
+    /* 
+     This function checks the exp required in order to get to the next level
+     */
     public int GetRequiredExp(int lvl)
     {
         requiredExp = (int)Math.Pow(lvl, 4) + 14;
         return requiredExp;
     }
 
+    /* 
+    This function addes the gained exp to the exp and the totalExp variables
+     
+    The exp variable is for tracking the required exp to level up
+     
+    The totalExp varibale is for tracking the total about exp gained across all levels
+     */
     public void GainExp(int num)
     {
         totalExp += num;
@@ -33,23 +47,34 @@
         }
     }
 
+    /* 
+     This function is called to calculate the exp that is required to level up
+     */
     public void CalculateRequiredExp()
     {
         requiredExp = GetRequiredExp(lvl);
     }
 
+    /* 
+     This handles all of the things that happen when you level up such as stat increases and all that jazz
+     */
     public void LevelUp()
     {
-        int randomHp = rnd.Next(4, 7);
-        int randomAtk = rnd.Next(1, 4);
-        int randomDef = rnd.Next(1, 4);
+        /* 
+        Please ignore all the random stat stuff i was testing something
+        */
+        //int randomHp = rnd.Next(4, 7);
+        //int randomAtk = rnd.Next(1, 4);
+        //int randomDef = rnd.Next(1, 4);
 
-        hp += randomHp;
-        atk += randomAtk;
-        def += randomDef;
+        //hp += randomHp;
+        //atk += randomAtk;
+        //def += randomDef;
+        
         lvl++;
 
         Console.WriteLine("You leveled up!\nYou are now level " + lvl);
+
         //Console.WriteLine("Hp increased by " + randomHp + "!\nHP: " + hp);
         //Console.WriteLine("Attack increased by " + randomAtk + "!\nAttack: " + atk);
         //Console.WriteLine("Defense increased by " + randomDef + "!\nDefense: " + def);
@@ -58,6 +83,9 @@
 
 class Program
 {
+    /* 
+     This is for yeilding text and dialouge
+     */
     static void Yeild()
     {
         Console.ReadLine();
@@ -66,7 +94,12 @@ class Program
     static void Main(string[] args)
     {
         Unit unit = new Unit();
-        
+
+        /* 
+        When you press enter, you gain exp (adjustable in the GainExp() call) and displayes your level and total exp
+
+        Once you reach level 20, you max out
+        */
         while (Console.ReadKey().Key == ConsoleKey.Enter)
         {
             unit.CalculateRequiredExp();
